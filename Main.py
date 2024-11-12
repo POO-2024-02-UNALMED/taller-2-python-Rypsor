b6e9dfac7928a46bd04ffbff57fc561458d3c3a1
Rypsor <samuelrb34@gmail.com>
2024-11-12 00:12:08 -0500

    prueba 19

--- Main.py
+++ Main.py
@@ -1,59 +1,74 @@
 class Asiento:
-    def __init__(self, color, registro):
-        self.color = color
-        self.registro = registro
+     #Con el inicializador creo los atributos de instancia
+     def __init__(self,color,precio,registro):
+         self.color = color
+         self.precio = precio
+         self.registro = registro
 
-    def cambiarColor(self, color_elegido):
-        if color_elegido in ("rojo","verde","amarillo","negro","blanco"):
-            self.color = color_elegido
-
-
-class Motor:
-    def __init__(self, numeroCilindros, tipo, registro):
-        self.numeroCilindros = numeroCilindros
-        self.tipo = tipo
-        self.registro = registro
-    
-    def cambiarRegistro(self, reg):
-        self.registro = reg
-
-    def asignarTipo(self, tip):
-        if tip == "electrico":
-            self.tipo = tip
-        elif tip == "gasolina":
-            self.tipo = tip
-        else:
-            pass
-
+     #Los metodos de instancia llevan siempre como primer parametro la palabra self
+     def cambiarColor(self, color):
+        if color == "rojo" or color == "verde" or color == "amarillo" or color == "negro" or color == "blanco":
+             #luego de verificar que este entre los permitidos
+             #el atributo color del objeto tendra el valor de color 
+             self.color = color
 
 class Auto:
-    cantidadCreados = 0
-    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+     #Atributo de clase
+     cantidadCreados = 0
+     
+     #Inicializador, atributos de instancia
+     def __init__(self, modelo, precio, asientos, marca, motor, registro):
         self.modelo = modelo
         self.precio = precio
+        #Si bien el diagrama UML indica que el atributo de asientos es un array
+        #Como python no es fuertemente tipado, no debemos indicar que asientos es un array
         self.asientos = asientos
+
         self.marca = marca
+        #Al atributo motor, estamos asignando un objeto de la clase Motor
         self.motor = motor
         self.registro = registro
 
-    def cantidadAsientos(self):
-        contador = 0
-        for i in self.asientos:
-            if isinstance(i,Asiento):
-                contador += 1
-        return contador
-    
-    def verificarIntegridad(self):
-        reg_auto = self.registro
-        if reg_auto != self.motor.registro:
-            return "Las piezas no son originales"
+
+
+     def cantidadAsientos(self):
+        cantidad = 0
+        #Si bien, no indicamos que asientos sea un array
+        #el in significa tambien recorrer los caracteres que conforman una palabra, por eso no bota error
+        for elemento in self.asientos:
             
-        else:
-            verificador = 0
-            for asiento in self.asientos:
-                if asiento.registro != reg_auto:
-                    verificador = 1
-            if verificador == 0:
-                return "Auto original"
-            else:
-                return "Las piezas no son originales"
+            #Usando la función type
+            #Comprobamos que cada elemento del atributo asientos, sea el tipo de dato Asientos(quien es una clase) 
+            if type(elemento) == Asiento:
+                cantidad += 1
+        return (cantidad)
+     
+     def verificarIntegridad(self):
+        #Los metodos de instancia los usan instancias/objetos, por eso siempre llevan el self
+        #Ese self, se reemplaza por el objeto que esta llamando al metodo
+        #nombre_del_objeto.nombre_del_atributo
+        #Como el atributo motor es tambien un objeto, escribimos el atributo que queremos acceder
+        if self.registro == self.motor.registro:
+            for elemento in self.asientos:
+                if elemento.registro != self.registro:
+                    return("Las piezas no son originales")
+                else:
+                    return("Auto original")
+        #Como el if no fue verdad, entonces retorne el siguiente texto
+        return("Las piezas no son originales")
+
+class Motor:
+     def __init__(self,numeroCilindros,tipo,registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+     
+     def cambiarRegistro(self,registro):
+         self.registro = registro
+
+     def asignarTipo(self,tipo):
+         if tipo == "electrico" or tipo == "gasolina":
+             self.tipo = tipo
+
+     
+        
